b5797fea302c4c7991b4468a38462e3e219c25f9
WillingDev0812 <greendev1004@gmail.com>
2024-07-20 01:04:50 +0300

    + Splash Screen Finished UI & Logic.

--- src/main/java/com/iti/tictactoe/MainApplication.java
+++ src/main/java/com/iti/tictactoe/MainApplication.java
@@ -1,20 +1,34 @@
 package com.iti.tictactoe;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader splashLoader = new FXMLLoader(MainApplication.class.getResource("/com/iti/tictactoe/Splash.fxml"));
+        //FXMLLoader mainLoader = new FXMLLoader(HelloApplication.class.getResource("/com/iti/tictacton_game/MainScreen.fxml"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(splashLoader.load());
+        //root.getChildren().add(mainLoader.load());
+
+
+        Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
+
+        PauseTransition pause = new PauseTransition(Duration.seconds(5));
+        pause.setOnFinished(event -> root.getChildren().get(0).setVisible(false));
+        pause.play();
     }
 
     public static void main(String[] args) {
