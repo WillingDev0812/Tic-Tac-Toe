4056e064d810548b4f3d2b58ee42332a410c36eb
WillingDev0812 <greendev1004@gmail.com>
2024-07-22 03:55:41 +0300

    completed LoginScreen navigation

--- src/main/java/com/iti/tictactoe/LoginScreen.java
+++ src/main/java/com/iti/tictactoe/LoginScreen.java
@@ -1,6 +1,7 @@
 package com.iti.tictactoe;
 
 import com.iti.tictactoe.muliplayerOffline.models.UiUtils;
+import com.iti.tictactoe.navigation.NavigationController;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,35 +9,46 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Border;
+import javafx.scene.paint.Color;
 
 public class LoginScreen {
 
     @FXML
-    private Button loginButton;
+    private Button loginBtn;
     @FXML
-    private Button signUpButton;
+    private Button signupBtn;
     @FXML
     private TextField emailTextField;
     @FXML
     private TextField passwordTextField ;
        @FXML
    private Label warningTextlabel;
+    private NavigationController navController;
 
 
     @FXML
     private void initialize() {
-        UiUtils.addHoverAnimation(loginButton);
-        UiUtils.addHoverAnimation(signUpButton);
+        UiUtils.addHoverAnimation(loginBtn);
+        UiUtils.addHoverAnimation(signupBtn);
     }
     
     public void onBackImageClick(MouseEvent mouseEvent) {
-        Platform.exit();
+        if (navController == null) {
+            System.out.println("error in navController");
+        } else {
+            UiUtils.playSoundEffect();
+            navController.popScene();
+        }
     }
 
     public void onLoginBtn(ActionEvent actionEvent) {
        if(emailTextField.getText().isEmpty() || passwordTextField.getText().isEmpty()) {
            //  emailTextField.borderProperty();
            warningTextlabel.setOpacity(1.0);
+           passwordTextField.setStyle("-fx-border-color: red ; -fx-border-width: 3px ; -fx-border-radius: 30; -fx-background-radius: 30; -fx-border-image-width: 5;" );
+           emailTextField.setStyle("-fx-border-color: red ; -fx-border-width: 3px ; -fx-border-radius: 30; -fx-background-radius: 30; -fx-border-image-width: 5;" );
+           //passwordTextField.setBorder(Border.stroke(Color.RED));
        }
         System.out.println(emailTextField.getText());
         System.out.println(passwordTextField.getText());
@@ -45,4 +57,10 @@ public class LoginScreen {
     public void onSignupBtn(ActionEvent actionEvent) {
         // implements late ...
     }
+
+
+    public void setNavController(NavigationController navController) {
+        this.navController = navController;
+    }
+
 }
