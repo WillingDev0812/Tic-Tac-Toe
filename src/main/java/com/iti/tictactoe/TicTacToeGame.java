2a68c06bdbb96178e1710ef4395e67fa18a45e2f
WillingDev0812 <greendev1004@gmail.com>
2024-07-27 13:33:54 +0300

    fixing unexpected close for the application

--- src/main/java/com/iti/tictactoe/TicTacToeGame.java
+++ src/main/java/com/iti/tictactoe/TicTacToeGame.java
@@ -1,6 +1,5 @@
 package com.iti.tictactoe;
 
-import com.iti.tictactoe.models.UiUtils;
 import com.iti.tictactoe.navigation.NavigationController;
 import javafx.animation.PauseTransition;
 import javafx.application.Application;
@@ -19,7 +18,9 @@ public class TicTacToeGame extends Application {
 
     @Override
     public void stop() throws Exception {
+        SocketManager socketManager = SocketManager.getInstance();
         new ListOfUsers().logout();
+        socketManager.close();
         super.stop();
     }
 
@@ -28,7 +29,7 @@ public class TicTacToeGame extends Application {
         try {
             // Initialize Navigation Controller
             navController = new NavigationController(stage);
-          //  UiUtils.playBackgroundMusic();
+            //  UiUtils.playBackgroundMusic();
 
             // Load and display the splash screen
             FXMLLoader splashLoader = new FXMLLoader(TicTacToeGame.class.getResource("/com/iti/tictactoe/splash.fxml"));
@@ -38,20 +39,18 @@ public class TicTacToeGame extends Application {
             Scene splashScene = new Scene(root);
             stage.setScene(splashScene);
 
-            stage.setFullScreen(true);
+            stage.setFullScreen(false);
             stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH); // Disable ESC to exit full-screen
 
             stage.show();
 
             // Transition to the home screen after a delay
             PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
-            pause.setOnFinished(event -> {
-                navController.pushScene("/com/iti/tictactoe/home-screen.fxml", controller -> {
-                    if (controller instanceof HomeScreenController homeScreenController) {
-                        homeScreenController.setNavController(navController);
-                    }
-                });
-            });
+            pause.setOnFinished(event -> navController.pushScene("/com/iti/tictactoe/home-screen.fxml", controller -> {
+                if (controller instanceof HomeScreenController homeScreenController) {
+                    homeScreenController.setNavController(navController);
+                }
+            }));
             pause.play();
         } catch (IOException e) {
             e.printStackTrace();
